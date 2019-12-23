9dfc8a39a4f9f829334f3f824f01a7a33759c688
raghurajj <raghurajpratap1999@gmail.com>
2019-12-23 12:14:54 +0530

    fetched location from navigator

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,13 @@ document.querySelector('#messageForm').addEventListener('submit',(e)=>{
    const message = e.target.elements.message.value
    socket.emit('sendMessage',message)
 })
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+    if(!navigator.geolocation)
+    {
+        return alert('Geolocation is not supported by your browser')   
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        console.log(position)
+    })
+})
