dd4a981f2132b8d903aa8a42f975e0e35b60ea63
raghurajj <raghurajpratap1999@gmail.com>
2019-12-26 09:56:44 +0530

    traking users joining/leaving

--- public/js/chat.js
+++ public/js/chat.js
@@ -67,4 +67,11 @@ $locationButton.addEventListener('click',()=>{
     })
 })
 
-socket.emit('join',{username,room})
+socket.emit('join',{username,room},(error)=>{
+
+    if(error)
+    {
+        alert(error)
+        location.href='/'
+    }
+})
